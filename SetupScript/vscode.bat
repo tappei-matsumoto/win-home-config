59e218e006eedeae28082a0ba030fce60144f66f
matsumoto_tatsuhei <matsumoto_tatsuhei@intec.co.jp>
2019-03-23 18:11:41 +0900

    vscodeのパスが間違っていたので修正

--- SetupScript/vscode.bat
+++ SetupScript/vscode.bat
@@ -4,4 +4,4 @@ echo vsocodeのセットアップ
 set BIN=%USERPROFILE%\bin
 
 echo vscodeをランチャーに追加
-cmd /c %BIN%\tools\mksc.bat "%BIN%\vscode.lnk" "%BIN%\vscode\cmd\Code.exe"
+cmd /c %BIN%\tools\mksc.bat "%BIN%\vscode.lnk" "%BIN%\vscode\Code.exe"
