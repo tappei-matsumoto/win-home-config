2c51b6f93e8f29c89a913dce4130f89bf0c1f567
matsumoto_tatsuhei <matsumoto_tatsuhei@intec.co.jp>
2020-01-08 08:46:54 +0900

    add yamy setupscript.

---
+++ SetupScript/yamy.bat
@@ -0,0 +1,11 @@
+@echo off
+echo yamyのセットアップ
+
+set BIN_FOLDER=%USERPROFILE%\bin
+set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+
+echo yamyをスタートアップに登録する
+cmd /c mksc "%STARTUP_FOLDER%\yamy.lnk" "%BIN_FOLDER%\yamy\yamy.exe"
+
+pause
+
